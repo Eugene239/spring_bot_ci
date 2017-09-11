ee59b33d94032daac6a50f2334fe4e06d4fda388
Eugene239 <adler.18@mail.ru>
2017-09-12 00:20:46 +0300

    Initial commit

---
+++ src/main/java/ru/epavlov/Application.java
@@ -0,0 +1,10 @@
+package ru.epavlov;
+
+/**
+ * Created by Eugene on 12.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
