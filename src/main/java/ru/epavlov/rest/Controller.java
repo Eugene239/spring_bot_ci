66cf0aaa7b2c1cd704d02b25409a387ceed20ee2
Eugene239 <adler.18@mail.ru>
2017-09-24 01:12:02 +0300

    test

--- src/main/java/ru/epavlov/rest/Controller.java
+++ src/main/java/ru/epavlov/rest/Controller.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String sas(){
-        return "you mut die";
+        return "greeting traveler";
     }
 }
