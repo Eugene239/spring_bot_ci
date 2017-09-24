881387bad6177228adbdf20e5622906abcc710f0
Eugene239 <adler.18@mail.ru>
2017-09-24 22:49:47 +0300

    add answer

--- src/main/java/ru/epavlov/rest/Controller.java
+++ src/main/java/ru/epavlov/rest/Controller.java
@@ -3,8 +3,6 @@ package ru.epavlov.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 /**
  * Created by Eugene on 12.09.2017.
  */
@@ -15,11 +13,8 @@ public class Controller {
         return "greeting traveler";
     }
     @GetMapping("/stop")
-    public void stop(){
-        try {
-            Runtime.getRuntime().exec("export pid=`pgrep -f spring_bot_ci`; kill -9 $pid");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public String stop(){
+       System.exit(0);
+       return "Application stopped";
     }
 }
