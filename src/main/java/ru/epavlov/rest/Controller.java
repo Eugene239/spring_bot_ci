a70af4fb00192e3fc80175996a56312dbfdf744f
epavlov <epavlov@reksoft.ru>
2017-09-25 11:05:42 +0300

    add jsp

--- src/main/java/ru/epavlov/rest/Controller.java
+++ src/main/java/ru/epavlov/rest/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Eugene on 12.09.2017.
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @GetMapping("/")
-    public String sas(Model model){
-        model.addAttribute("kek","privet");
-        return "index";
+    public ModelAndView sas(){
+        ModelAndView mav= new ModelAndView("index");
+        //mav.addAttribute("kek","privet");
+        mav.addObject("kek","privet");
+        return mav;
     }
 
     @GetMapping("/stop")
